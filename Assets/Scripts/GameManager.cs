24fd4783f753eb059be874d5dc29e8f00c668597
sanghane <sanghane08@gmail.com>
2024-11-01 14:53:05 +0900

    4편 젤리성장
    스크립트대로 리스트 추가가 안됨

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int[] jellyGoldList;
     public Vector3[] PointList;
+    
+    public runtimeAnimatorController[] LevelAc;
+
+    public void ChangeAc(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = LevelAc[level-1];
+    }
     
 }
